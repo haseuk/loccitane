d9bf52ddf97d21dffd5aeced78d8239b7d72b946
haseuk <73511954+haseuk@users.noreply.github.com>
2021-05-18 15:27:52 +0900

    210518

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,32 @@
-var gulp = require('gulp');
-var argv = require('yargs').argv;
-var fileinclude = require('gulp-file-include');
-var htmlmin = require('gulp-htmlmin');
-var gulpsync = require('gulp-sync')(gulp);
+const gulp = require('gulp');
+const argv = require('yargs').argv;
+const fileinclude = require('gulp-file-include');
+const htmlmin = require('gulp-htmlmin');
 
 gulp.task('css', function () {
   return gulp.src(argv.folder + "/body.css", {base: "" + argv.folder})
-      .pipe(gulp.dest('temp/'));
+    .pipe(gulp.dest('temp/'));
 });
 
 gulp.task('html', function () {
   return gulp.src(argv.folder + "/body.html", {base: "" + argv.folder})
-      .pipe(gulp.dest('temp/'));
+    .pipe(gulp.dest('temp/'));
 });
 
 gulp.task('build', function () {
   return gulp.src("template/build.html", {base: "template"})
-      .pipe(fileinclude())
-      .pipe(gulp.dest("dist/" + argv.folder + "/"));
+    .pipe(fileinclude())
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("dist/" + argv.folder + "/"));
 });
 
 gulp.task('web', function () {
   return gulp.src("template/web.html", {base: "template"})
-      .pipe(fileinclude())
-      .pipe(gulp.dest("dist/" + argv.folder + "/"));
+    .pipe(fileinclude())
+    .pipe(gulp.dest("dist/" + argv.folder + "/"));
 });
 
-gulp.task('default', gulpsync.sync([
+gulp.task('default', gulp.series([
   ['css', 'html'],
   ['build', 'web'],
 ]));
